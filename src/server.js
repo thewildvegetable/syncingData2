7ea755ea45f1f90bddfb7705ad1b34f4e2296d58
Jeffrey Karger <whichazelnj13@aol.com>
2018-02-16 18:33:00 -0500

    Parts of click setup

--- src/server.js
+++ src/server.js
@@ -16,20 +16,46 @@ const app = http.createServer(onRequest).listen(port);
 
 console.log(`listening at 127.0.0.1: ${port}`);
 
+const square = {
+    x: Math.floor(Math.random()*(600) + 1),
+    y: Math.floor(Math.random()*(400) + 1),
+    width: 100,
+    height: 100,
+    color: "red"
+};
+
+let clicked = false;        //make true after the first click message to avoid making multiple setTimers
+let users = {};
+
+//make a new square
+const newSquare = () => {
+    square.x = Math.floor(Math.random()*(600) + 1);
+    square.y = Math.floor(Math.random()*(400) + 1);
+    square.color = `rgb(${Math.floor((Math.random()*255) + 1)},${Math.floor((Math.random()*255) + 1)},${Math.floor((Math.random()*255) + 1)})`;
+};
+
+//figure out which socket was the first submission
+const determineFirstSubmission = () => {
+    
+};
+
 const io = socketio(app);
 
 const onJoined = (sock) => {
   const socket = sock;
 
-  socket.on('join', () => {
+  socket.on('join', (user) => {
+      
     socket.join('room1');
+      users[user] = socket;
+      socket.emit('draw', square);
   });
 };
 
 const onUpdate = (sock) => {
   const socket = sock;
 
-  socket.on('draw', (data) => {
+  socket.on('click', (data) => {
     socket.broadcast.to('room1').emit('msg', data);
   });
 };
